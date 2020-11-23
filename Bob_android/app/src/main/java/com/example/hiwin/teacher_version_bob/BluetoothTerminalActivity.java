0024ab2563ac241a54f957c4edad06c25933193d
VincentYeh-dev <vincent3314534@gmail.com>
2020-11-23 23:43:39 +0800

    Basic terminal completed.

--- Bob_android/app/src/main/java/com/example/hiwin/teacher_version_bob/BluetoothTerminalActivity.java
+++ Bob_android/app/src/main/java/com/example/hiwin/teacher_version_bob/BluetoothTerminalActivity.java
@@ -1,5 +1,6 @@
 package com.example.hiwin.teacher_version_bob;
 
+import android.annotation.SuppressLint;
 import android.bluetooth.BluetoothAdapter;
 import android.bluetooth.BluetoothDevice;
 import android.content.ComponentName;
@@ -10,10 +11,13 @@ import android.os.IBinder;
 import android.speech.tts.TextToSpeech;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
 import android.text.Spannable;
 import android.text.SpannableStringBuilder;
 import android.text.style.ForegroundColorSpan;
 import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -37,6 +41,8 @@ public class BluetoothTerminalActivity extends AppCompatActivity implements Serv
             https://developer.android.com/guide/components/fragments
             https://developer.android.com/guide/components/services
             https://www.tutorialspoint.com/android/android_text_to_speech.htm
+            https://medium.com/verybuy-dev/android-%E8%A3%A1%E7%9A%84%E7%B4%84%E6%9D%9F%E6%80%96%E5%B1%80-constraintlayout-6225227945ab
+            https://materialdesignicons.com/
      */
 
     private enum Connected {False, Pending, True}
@@ -44,15 +50,18 @@ public class BluetoothTerminalActivity extends AppCompatActivity implements Serv
     private static final byte[] ENABLE_PATTERN_RECOGNITION = {(byte) 0xff, (byte) 0xef, (byte) 0x01, (byte) 0x00, (~1)};
 
 
-
     private TextView receiveText;
     private TextView sendText;
+    private Toolbar toolbar;
+    private MenuItem connection;
+
     private Context context;
+
 
     private String deviceAddress;
     private SerialService service;
 
-    private boolean initialStart=true;
+    private boolean initialStart = true;
     private Connected connected = Connected.False;
     private TextToSpeech textToSpeech;
 
@@ -60,12 +69,13 @@ public class BluetoothTerminalActivity extends AppCompatActivity implements Serv
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_bluetooth_terminal);
-
+        toolbar = (Toolbar) findViewById(R.id.term_toolbar);
+        setSupportActionBar(toolbar);
         context = this;
 
 //        OnAttach
-        boolean sus=bindService(new Intent(context, SerialService.class), this, Context.BIND_AUTO_CREATE);
-        Log.d("BindService",sus+"");
+        boolean sus = bindService(new Intent(context, SerialService.class), this, Context.BIND_AUTO_CREATE);
+        Log.d("BindService", sus + "");
 //        OnCreate
         Intent it = getIntent();
         deviceAddress = it.getStringExtra("address");
@@ -74,16 +84,36 @@ public class BluetoothTerminalActivity extends AppCompatActivity implements Serv
         receiveText = (TextView) findViewById(R.id.receive_text);
         sendText = (TextView) findViewById(R.id.send_text);
 
-        textToSpeech=new TextToSpeech(context, new TextToSpeech.OnInitListener() {
+        textToSpeech = new TextToSpeech(context, new TextToSpeech.OnInitListener() {
             @Override
             public void onInit(int status) {
-                if(status != TextToSpeech.ERROR) {
+                if (status != TextToSpeech.ERROR) {
                     textToSpeech.setLanguage(Locale.US);
                 }
             }
         });
 
 
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // 設置要用哪個menu檔做為選單
+        getMenuInflater().inflate(R.menu.menu_term, menu);
+        connection = menu.getItem(0);
+        return true;
+    }
+
+    @SuppressLint("NonConstantResourceId")
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if (item.getItemId() == R.id.menu_term_connection) {
+            if (connected == Connected.False)
+                connect();
+            else if (connected == Connected.True)
+                disconnect();
+        }
+        return super.onOptionsItemSelected(item);
     }
 
     @Override
@@ -108,7 +138,8 @@ public class BluetoothTerminalActivity extends AppCompatActivity implements Serv
             runOnUiThread(new Runnable() {
                 @Override
                 public void run() {
-                    connect();
+
+                    setMenuConnectionStatus(connected);
                 }
             });
         }
@@ -123,13 +154,16 @@ public class BluetoothTerminalActivity extends AppCompatActivity implements Serv
     public void onSerialConnect() {
         status("connected");
         connected = Connected.True;
+        setMenuConnectionStatus(connected);
     }
 
     @Override
     public void onSerialConnectError(Exception e) {
         status("connection failed: " + e.getMessage());
         e.printStackTrace();
+        connection.setIcon(R.drawable.link_off);
         disconnect();
+//        finish();
     }
 
     @Override
@@ -141,6 +175,7 @@ public class BluetoothTerminalActivity extends AppCompatActivity implements Serv
     public void onSerialIoError(Exception e) {
         status("connection lost: " + e.getMessage());
         disconnect();
+//        finish();
     }
 
     @Override
@@ -151,7 +186,7 @@ public class BluetoothTerminalActivity extends AppCompatActivity implements Serv
             runOnUiThread(new Runnable() {
                 @Override
                 public void run() {
-                    connect();
+//                    connect();
                 }
             });
         }
@@ -160,10 +195,11 @@ public class BluetoothTerminalActivity extends AppCompatActivity implements Serv
     @Override
     protected void onDestroy() {
         super.onDestroy();
-
+        if (textToSpeech != null)
+            textToSpeech.shutdown();
         if (connected != Connected.False)
             disconnect();
-        stopService(new Intent(context,SerialService.class));
+        stopService(new Intent(context, SerialService.class));
 
     }
 
@@ -191,7 +227,9 @@ public class BluetoothTerminalActivity extends AppCompatActivity implements Serv
     }
 
     private void disconnect() {
+        status("disconnected");
         connected = Connected.False;
+        setMenuConnectionStatus(connected);
         service.disconnect();
     }
 
@@ -202,7 +240,7 @@ public class BluetoothTerminalActivity extends AppCompatActivity implements Serv
         }
         try {
             byte[] data = msg.getBytes();
-            status("send:\t"+msg);
+            status("send:\t" + msg);
             textToSpeech.speak(msg, TextToSpeech.QUEUE_FLUSH, null);
             service.write(data);
         } catch (Exception e) {
@@ -212,13 +250,23 @@ public class BluetoothTerminalActivity extends AppCompatActivity implements Serv
 
     private void receive(byte[] data) {
         String msg = new String(data);
-        status("receive:\t"+msg);
+        status("receive:\t" + msg);
         textToSpeech.speak(msg, TextToSpeech.QUEUE_FLUSH, null);
 
     }
 
     void status(String str) {
         Log.d("BluetoothLog", str);
-        receiveText.append(str+"\n");
+        receiveText.append(str + "\n");
+    }
+
+    void setMenuConnectionStatus(Connected connected) {
+        if (connected==Connected.False) {
+            connection.setIcon(R.drawable.link);
+            connection.setTitle("Connected");
+        }else if(connected==Connected.True){
+            connection.setIcon(R.drawable.link_off);
+            connection.setTitle("Disconnected");
+        }
     }
 }
