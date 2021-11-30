bb24b2cf55d6f784ceff530e75f4a80148c471cb
VincentYeh-dev <vincent3314534@gmail.com>
2021-11-30 23:18:38 +0800

    Change face to detector class.

---
+++ Bob_python/detector/concrete/face.py
@@ -0,0 +1,54 @@
+import time
+import cv2
+from deepface import DeepFace
+from detector.framework.detector import Detector, DetectListener
+
+
+class FaceDetector(Detector):
+    def __init__(self, listener: DetectListener):
+        super().__init__(listener)
+        self.__start = False
+        self.__interrupt = False
+
+    def start(self):
+        self.__start = True
+
+    def pause(self):
+        self.__start = False
+
+    def stop(self):
+        self.__interrupt = True
+        pass
+
+    def detect(self):
+        faceCascade = cv2.CascadeClassifier(cv2.data.haarcascades + 'haarcascade_frontalface_default.xml')
+        cap = cv2.VideoCapture(0)
+        cap.set(cv2.CAP_PROP_BUFFERSIZE, 1)
+        while not self.__interrupt:
+            while not self.__start:
+                time.sleep(1)
+            ret, frame = cap.read()
+            cv2.imshow('raw', frame)
+            try:
+                result = DeepFace.analyze(frame, actions=['emotion'])
+                gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+
+                faces = faceCascade.detectMultiScale(gray, 1.1, 4)
+
+                for (x, y, w, h) in faces:
+                    cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 255, 0), 2)
+
+                font = cv2.FONT_HERSHEY_SIMPLEX
+
+                cv2.putText(frame, result['dominant_emotion'], (50, 50), font, 3, (0, 0, 255), 2, cv2.LINE_4)
+
+                cv2.imshow('result', frame)
+                face_type = result['dominant_emotion']
+                self.listener.onDetect(face_type)
+            except Exception as e:
+                print(e.__str__())
+                pass
+            if cv2.waitKey(2) & 0xFF == ord('q'):
+                break
+        cap.release()
+        cv2.destroyWindow()
--- Bob_python/face_detect.py
+++ Bob_python/face_detect.py
@@ -1,46 +1,40 @@
 import base64
 import json
 import os
-import platform
-import re
-import time
-from typing import Optional
+from typing import Optional, List
 
-import cv2
-from deepface import DeepFace
-from serial import SerialException, SerialTimeoutException
+from serial import SerialTimeoutException
 
 from bluetooth.concrete.package import Base64LinePackage, StringPackage
 from bluetooth.framework.monitor import SerialListener
 from bluetooth.framework.package import Package
-from dbctrl.concrete import queryJsonFromName
 from dbctrl.concrete.database import FileDatabase
 from dbctrl.concrete.json_data import JSONData, JSONDataParser
-from robotics.framework.action import CSVAction, Action
-from serial.tools.list_ports_linux import comports
+from detector.concrete.face import FaceDetector
+from detector.framework.detector import DetectListener, Detector
+from robotics.framework.action import Action
 from serial_utils import getRobot, getBluetooth
 
 
 class RobotSerialListener(SerialListener):
+    def __init__(self, d: Detector):
+        self.detector = d
 
     def onReceive(self, data: bytes):
-        global detect
         d = base64.decodebytes(data)
         cmd = d.decode()
         if cmd == "START_DETECT":
+            self.detector.start()
             print("Start detect")
-            detect = True
         elif cmd == "PAUSE_DETECT":
+            self.detector.pause()
             print("Pause detect")
-            detect = False
 
 
 db_location = f"db{os.path.sep}faces.json"
 db_charset = 'UTF-8'
 db = FileDatabase(open(db_location, encoding=db_charset), JSONDataParser())
 robot = getRobot()
-bt = getBluetooth(RobotSerialListener())
-detect = True
 robot_done = True
 bt_done = True
 
@@ -65,45 +59,22 @@ def pushDataToBluetooth(package: Package):
         bt_done = True
 
 
-faceCascade = cv2.CascadeClassifier(cv2.data.haarcascades + 'haarcascade_frontalface_default.xml')
-cap = cv2.VideoCapture(0)
-cap.set(cv2.CAP_PROP_BUFFERSIZE, 1)
-
-while True:
-    while not detect:
-        time.sleep(1)
-
-    ret, frame = cap.read()
-    cv2.imshow('raw', frame)
-
-    try:
-        result = DeepFace.analyze(frame, actions=['emotion'])
-        gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-
-        faces = faceCascade.detectMultiScale(gray, 1.1, 4)
-
-        for (x, y, w, h) in faces:
-            cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 255, 0), 2)
-
-        font = cv2.FONT_HERSHEY_SIMPLEX
-
-        cv2.putText(frame, result['dominant_emotion'], (50, 50), font, 3, (0, 0, 255), 2, cv2.LINE_4)
-
-        cv2.imshow('result', frame)
-        face_type = result['dominant_emotion']
+class AListener(DetectListener):
+    def onDetect(self, face_type: str):
         print("now face emotion: " + face_type)
-
         obj: Optional[JSONData] = db.queryForId(face_type)
         if obj is not None:
             data: json = obj.getData()
             jsonString = json.dumps(data, ensure_ascii=False)
             print(jsonString)
             pushDataToBluetooth(Base64LinePackage(StringPackage(jsonString, 'UTF-8')))
-    except Exception as e:
-        print(e.__str__())
-        pass
-    if cv2.waitKey(2) & 0xFF == ord('q'):
-        break
 
-cap.release()
-cv2.destroyWindow()
+
+detector = FaceDetector(AListener())
+bt = getBluetooth(RobotSerialListener(detector))
+try:
+    detector.detect()
+except KeyboardInterrupt as e:
+    print("Interrupted!!")
+    detector.stop()
+    bt.close()
