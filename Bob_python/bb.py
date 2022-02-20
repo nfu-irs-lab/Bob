bcbe7984214d43490b505a057d07b2ffe0487580
VincentYeh-dev <vincent3314534@gmail.com>
2022-02-20 18:08:04 +0800

    test servos.

--- Bob_python/bb.py
+++ Bob_python/bb.py
@@ -1,17 +1,18 @@
+import time
+
 from Bob.robot.concrete.crt_dynamixel import DynamixelRobot
-from Bob.robot.concrete.crt_dynamixel_servo import RX_64
+from Bob.robot.concrete.crt_dynamixel_servo import RX_64, H42_20_S300_R
 
-s1 = RX_64(1, 1)
-s2 = RX_64(3, 1)
-s3 = RX_64(5, 1)
-s4 = RX_64(7, 1)
-servos = {1: s1, 3: s2, 5: s3, 7: s4}
-
+s1 = H42_20_S300_R(1, 2)
+s2 = RX_64(2, 1)
 robot = DynamixelRobot('/dev/ttyUSB0', 115200)
-robot.setVelocity(servos[1], 1023)
-robot.setVelocity(servos[3], 1023)
-robot.setVelocity(servos[5], 1023)
-robot.setVelocity(servos[7], 1023)
 robot.open()
-
+robot.enableToque(s1,True)
+robot.enableToque(s2,True)
+robot.setGoalPosition(s1, 0)
+robot.setGoalPosition(s2, 0)
+time.sleep(3)
+robot.setVelocity(s1,70000)
+robot.setGoalPosition(s1,70000)
+robot.setGoalPosition(s2, 1023)
 robot.close()
