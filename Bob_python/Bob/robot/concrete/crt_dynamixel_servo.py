741a7fa577d618fbf0890977e0f0850784b1eed5
VincentYeh-dev <vincent3314534@gmail.com>
2022-02-21 17:39:28 +0800

    Add MX-64,RX-24F.

--- Bob_python/Bob/robot/concrete/crt_dynamixel_servo.py
+++ Bob_python/Bob/robot/concrete/crt_dynamixel_servo.py
@@ -45,6 +45,51 @@ class RX_64(DynamixelServo):
 
 class MX_106(DynamixelServo):
 
+    def __init__(self, servoId: int, protocol: int):
+        super().__init__(servoId, protocol)
+
+    def getGoalPositionAddressLength(self):
+        return {'address': 30, 'length': 2}
+
+    def getPresentPositionAddressLength(self):
+        return {'address': 36, 'length': 2}
+
+    def getGoalVelocityAddressLength(self):
+        return {'address': 32, 'length': 2}
+
+    def getTorqueEnableAddressLength(self):
+        return {'address': 24, 'length': 1}
+
+    def getMovingAddressLength(self):
+        return {'address': 46, 'length': 1}
+
+
+class MX_64(DynamixelServo):
+
+    def __init__(self, servoId: int, protocol: int):
+        super().__init__(servoId, protocol)
+
+    def getGoalPositionAddressLength(self):
+        return {'address': 30, 'length': 2}
+
+    def getPresentPositionAddressLength(self):
+        return {'address': 36, 'length': 2}
+
+    def getGoalVelocityAddressLength(self):
+        return {'address': 32, 'length': 2}
+
+    def getTorqueEnableAddressLength(self):
+        return {'address': 24, 'length': 1}
+
+    def getMovingAddressLength(self):
+        return {'address': 46, 'length': 1}
+
+
+class RX_24F(DynamixelServo):
+
+    def __init__(self, servoId: int, protocol: int):
+        super().__init__(servoId, protocol)
+
     def getGoalPositionAddressLength(self):
         return {'address': 30, 'length': 2}
 
--- Bob_python/Bob/robot/concrete/servo_agent.py
+++ Bob_python/Bob/robot/concrete/servo_agent.py
@@ -1,6 +1,7 @@
 import csv
 from typing import List
 
+from Bob.robot.concrete.crt_dynamixel import PROTOCOL_1, PROTOCOL_2
 from Bob.robot.concrete.crt_dynamixel_servo import *
 
 
@@ -26,20 +27,26 @@ class CSVServoAgent:
 
             return servoList
 
-    def _getProtocol(self, string: str):
+    @staticmethod
+    def _getProtocol(string: str):
         if string == "1.0":
-            return 1
+            return PROTOCOL_1
         elif string == "2.0":
-            return 2
+            return PROTOCOL_2
         else:
             raise Exception("Unknown protocol")
 
-    def _getModel(self, model: str, _id: int, protocol: int) -> DynamixelServo:
+    @staticmethod
+    def _getModel(model: str, _id: int, protocol: int) -> DynamixelServo:
         if model == "RX-64":
             return RX_64(_id, protocol)
         elif model == "MX-106":
             return MX_106(_id, protocol)
         elif model == "H42-20-S300-R":
             return H42_20_S300_R(_id, protocol)
+        elif model == "MX-64":
+            return MX_64(_id, protocol)
+        elif model == "RX-24F":
+            return RX_24F(_id, protocol)
         else:
             raise Exception("Unknown model")
