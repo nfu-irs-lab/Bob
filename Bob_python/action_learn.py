ecb8770996e31ce8c393a333ee6555355e0684cb
VincentYeh-dev <vincent3314534@gmail.com>
2022-02-23 10:04:57 +0800

    Reset before learn action.

--- Bob_python/action_learn.py
+++ Bob_python/action_learn.py
@@ -2,6 +2,7 @@ from Bob.robot.concrete.crt_dynamixel import Dynamixel
 from Bob.robot.concrete.crt_robot import DynamixelRobotAdaptor
 from Bob.robot.concrete.servo_agent import CSVServoAgent
 from Bob.serial_config import getSerialNameByDescription
+from constants import getCommandsFromFileName
 
 bot_description = ".*FT232R.*"
 agent = CSVServoAgent("servos.csv")
@@ -10,6 +11,9 @@ for servo in agent.getDefinedServos():
     dynamixel.appendServo(servo)
 
 dynamixel.open()
+robot = DynamixelRobotAdaptor(dynamixel)
+robot.doCommands(getCommandsFromFileName('reset.csv'))
+
 for servo in agent.getDefinedServos():
     dynamixel.enableTorque(servo.getId(), False)
 print("Enable torque")
