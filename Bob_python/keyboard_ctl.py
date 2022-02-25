d798b01201db2126ffbaadb886625a6b3247c0c0
VincentYeh-dev <vincent3314534@gmail.com>
2022-02-25 13:46:47 +0800

    Fix keyboard_ctl.py

--- Bob_python/keyboard_ctl.py
+++ Bob_python/keyboard_ctl.py
@@ -1,33 +1,42 @@
 import keyboard  # using module keyboard
 
-from Bob.robot.concrete.crt_command import DynamixelCommand
+from Bob.robot.concrete.crt_command import *
 from Bob.robot.concrete.crt_dynamixel import Dynamixel
 from Bob.robot.concrete.crt_robot import DynamixelRobotAdaptor
 from Bob.robot.concrete.servo_agent import CSVServoAgent
+from Bob.serial_config import getSerialNameByDescription
 
+bot_description = ".*FT232R.*"
 agent = CSVServoAgent("servos.csv")
-dynamixel = Dynamixel('/dev/ttyUSB0', 115200)
+dynamixel = Dynamixel(getSerialNameByDescription(bot_description), 115200)
 for servo in agent.getDefinedServos():
     dynamixel.appendServo(servo)
 
 robot = DynamixelRobotAdaptor(dynamixel)
 robot.open()
-robot.init()
+
+robot.doCommand(DynamixelTorqueEnableCommand(11, True))
+robot.doCommand(DynamixelTorqueEnableCommand(12, True))
+keyboard.on_press_key("w", lambda _: walk(1))
+keyboard.on_press_key("s", lambda _: walk(2))
+keyboard.on_press_key("d", lambda _: walk(3))
+keyboard.on_press_key("a", lambda _: walk(4))
+
+
+def walk(direction: int):
+    if direction == 1:
+        robot.doCommand(DynamixelVelocityCommand(11, -10000))
+        robot.doCommand(DynamixelVelocityCommand(12, 10000))
+        print("walk forward")
+    elif direction == 2:
+        robot.doCommand(DynamixelVelocityCommand(11, 10000))
+        robot.doCommand(DynamixelVelocityCommand(12, -10000))
+        print("walk backward")
+    elif direction == 3:
+        print("turn right")
+    elif direction == 4:
+        print("turn left")
+
 
 while True:
-    i = input()
-    if i == "w":
-        print("walk forward")
-        robot.doCommand(DynamixelCommand(1, 0, 10000))
-    if i == "s":
-        print("walk backward")
-        robot.doCommand(DynamixelCommand(1, 0, -10000))
-    if i == "d":
-        print("turn right")
-    if i == "a":
-        print("turn left")
-    if i == "q":
-        break
-
-robot.doCommand(DynamixelCommand(1, 0, 0))
-robot.close()
+    pass
