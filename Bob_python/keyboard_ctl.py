b270b3be91e29868100fee781cc654a8b523fcf2
VincentYeh-dev <vincent3314534@gmail.com>
2022-03-09 18:35:15 +0800

    Complete keyboard_ctl.py

--- Bob_python/keyboard_ctl.py
+++ Bob_python/keyboard_ctl.py
@@ -6,50 +6,110 @@ from device_config import getRobot
 robot = getRobot()
 robot.open()
 robot.enableAllServos(True)
-keyboard.on_press_key("w", lambda _: walk(1))
-keyboard.on_press_key("s", lambda _: walk(2))
-keyboard.on_press_key("d", lambda _: walk(3))
-keyboard.on_press_key("a", lambda _: walk(4))
+
+w_lock = False
+keyboard.on_press_key("w", lambda _: key_w_press())
+keyboard.on_release_key("w", lambda _: key_w_release())
+
+s_lock = False
+keyboard.on_press_key("s", lambda _: key_s_press())
+keyboard.on_release_key("s", lambda _: key_s_release())
+
+d_lock = False
+keyboard.on_press_key("d", lambda _: key_d_press())
+keyboard.on_release_key("d", lambda _: key_d_release())
+
+a_lock = False
+keyboard.on_press_key("a", lambda _: key_a_press())
+keyboard.on_release_key("a", lambda _: key_a_release())
+
 keyboard.on_press_key(" ", lambda _: stop())
-keyboard.on_press_key("q", lambda _: exit(1))
 
 
 def stop():
-    robot.doCommand(DynamixelVelocityCommand(11, 0))
-    robot.doCommand(DynamixelVelocityCommand(12, 0))
+    print("Stop")
+    left_ctl(0)
+    right_ctl(0)
 
-    robot.doCommand(DynamixelVelocityCommand(13, 0))
-    robot.doCommand(DynamixelVelocityCommand(14, 0))
+
+def right_ctl(velocity: int):
+    robot.doCommand(DynamixelVelocityCommand(12, velocity))
+    robot.doCommand(DynamixelVelocityCommand(13, velocity))
+
+
+def left_ctl(velocity: int):
+    robot.doCommand(DynamixelVelocityCommand(11, velocity))
+    robot.doCommand(DynamixelVelocityCommand(14, velocity))
+
+
+def key_w_press():
+    global w_lock
+    if not w_lock:
+        w_lock = True
+        walk(1)
+
+
+def key_w_release():
+    global w_lock
+    w_lock = False
+    stop()
+
+
+def key_s_press():
+    global s_lock
+    if not s_lock:
+        s_lock = True
+        walk(2)
+
+
+def key_s_release():
+    global s_lock
+    s_lock = False
+    stop()
+
+
+def key_d_press():
+    global d_lock
+    if not d_lock:
+        d_lock = True
+        walk(3)
+
+
+def key_d_release():
+    global d_lock
+    d_lock = False
+    stop()
+
+
+def key_a_press():
+    global a_lock
+    if not a_lock:
+        a_lock = True
+        walk(4)
+
+
+def key_a_release():
+    global a_lock
+    a_lock = False
+    stop()
 
 
 def walk(direction: int):
     if direction == 1:
-        robot.doCommand(DynamixelVelocityCommand(11, 10000))
-        robot.doCommand(DynamixelVelocityCommand(12, 10000))
-
-        robot.doCommand(DynamixelVelocityCommand(13, -10000))
-        robot.doCommand(DynamixelVelocityCommand(14, -10000))
+        right_ctl(-5000)
+        left_ctl(5000)
         print("walk forward")
     elif direction == 2:
-        robot.doCommand(DynamixelVelocityCommand(11, -10000))
-        robot.doCommand(DynamixelVelocityCommand(12, -10000))
-
-        robot.doCommand(DynamixelVelocityCommand(13, 10000))
-        robot.doCommand(DynamixelVelocityCommand(14, 10000))
+        right_ctl(5000)
+        left_ctl(-5000)
         print("walk backward")
     elif direction == 3:
-        robot.doCommand(DynamixelVelocityCommand(11, 10000))
-        robot.doCommand(DynamixelVelocityCommand(12, 10000))
-
-        robot.doCommand(DynamixelVelocityCommand(13, -2000))
-        robot.doCommand(DynamixelVelocityCommand(14, -2000))
+        right_ctl(-5000)
+        left_ctl(-5000)
         print("turn right")
     elif direction == 4:
-        robot.doCommand(DynamixelVelocityCommand(11, 2000))
-        robot.doCommand(DynamixelVelocityCommand(12, 2000))
-
-        robot.doCommand(DynamixelVelocityCommand(13, -10000))
-        robot.doCommand(DynamixelVelocityCommand(14, -10000))
+        right_ctl(5000)
+        left_ctl(5000)
         print("turn left")
 
 
