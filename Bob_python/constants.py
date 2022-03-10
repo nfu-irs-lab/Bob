45777f9f518d7b19b554b661941185f2a56ffbeb
VincentYeh-dev <vincent3314534@gmail.com>
2022-03-10 23:20:48 +0800

    Make keyboard_ctl.py be a class.

--- Bob_python/constants.py
+++ Bob_python/constants.py
@@ -16,6 +16,7 @@ from Bob.detector.framework.detector import DetectListener
 from Bob.robot.concrete.crt_command import DynamixelVelocityCommand
 from command_utils import getCommandsFromFileName
 from device_config import getRobot
+from keyboard_ctl import KeyboardController
 
 obj_db_location = f"db{os.path.sep}objects.json"
 face_db_location = f"db{os.path.sep}faces.json"
@@ -34,116 +35,8 @@ robot.open()
 
 robot.enableAllServos(True)
 
-w_lock = False
-s_lock = False
-d_lock = False
-
-a_lock = False
-
-
-def stop():
-    print("Stop")
-    left_ctl(0)
-    right_ctl(0)
-
-
-def right_ctl(velocity: int):
-    robot.doCommand(DynamixelVelocityCommand(12, velocity))
-    robot.doCommand(DynamixelVelocityCommand(13, velocity))
-
-
-def left_ctl(velocity: int):
-    robot.doCommand(DynamixelVelocityCommand(11, velocity))
-    robot.doCommand(DynamixelVelocityCommand(14, velocity))
-
-
-def key_w_press():
-    global w_lock
-    if not w_lock:
-        w_lock = True
-        walk(1)
-
-
-def key_w_release():
-    global w_lock
-    w_lock = False
-    stop()
-
-
-def key_s_press():
-    global s_lock
-    if not s_lock:
-        s_lock = True
-        walk(2)
-
-
-def key_s_release():
-    global s_lock
-    s_lock = False
-    stop()
-
-
-def key_d_press():
-    global d_lock
-    if not d_lock:
-        d_lock = True
-        walk(3)
-
-
-def key_d_release():
-    global d_lock
-    d_lock = False
-    stop()
-
-
-def key_a_press():
-    global a_lock
-    if not a_lock:
-        a_lock = True
-        walk(4)
-
-
-def key_a_release():
-    global a_lock
-    a_lock = False
-    stop()
-
-
-def walk(direction: int):
-    if direction == 1:
-        right_ctl(-5000)
-        left_ctl(5000)
-        print("walk forward")
-    elif direction == 2:
-        right_ctl(5000)
-        left_ctl(-5000)
-        print("walk backward")
-    elif direction == 3:
-        right_ctl(5000)
-        left_ctl(5000)
-        print("turn right")
-    elif direction == 4:
-        right_ctl(-5000)
-        left_ctl(-5000)
-        print("turn left")
-
-
-def keyboard_ctl():
-    keyboard.on_press_key("w", lambda _: key_w_press())
-    keyboard.on_release_key("w", lambda _: key_w_release())
-    keyboard.on_press_key("s", lambda _: key_s_press())
-    keyboard.on_release_key("s", lambda _: key_s_release())
-    keyboard.on_press_key("d", lambda _: key_d_press())
-    keyboard.on_release_key("d", lambda _: key_d_release())
-    keyboard.on_press_key("a", lambda _: key_a_press())
-    keyboard.on_release_key("a", lambda _: key_a_release())
-    keyboard.on_press_key(" ", lambda _: stop())
-    while True:
-        pass
-
-
-th = threading.Thread(target=keyboard_ctl)
-th.start()
+keyboard_ctl = KeyboardController(robot)
+keyboard_ctl.init()
 
 
 def formatDataToJsonString(id: int, type: str, content: str, data):
--- Bob_python/keyboard_ctl.py
+++ Bob_python/keyboard_ctl.py
@@ -1,120 +1,132 @@
 import keyboard  # using module keyboard
 
 from Bob.robot.concrete.crt_command import *
+from Bob.robot.framework.fw_robot import Robot
+from command_utils import getCommandsFromFileName
 from device_config import getRobot
 
-robot = getRobot()
-robot.open()
-robot.enableAllServos(True)
-
-w_lock = False
-keyboard.on_press_key("w", lambda _: key_w_press())
-keyboard.on_release_key("w", lambda _: key_w_release())
-
-s_lock = False
-keyboard.on_press_key("s", lambda _: key_s_press())
-keyboard.on_release_key("s", lambda _: key_s_release())
-
-d_lock = False
-keyboard.on_press_key("d", lambda _: key_d_press())
-keyboard.on_release_key("d", lambda _: key_d_release())
-
-a_lock = False
-keyboard.on_press_key("a", lambda _: key_a_press())
-keyboard.on_release_key("a", lambda _: key_a_release())
-
-keyboard.on_press_key(" ", lambda _: stop())
+VOL = 4000
 
 
-def stop():
-    print("Stop")
-    left_ctl(0)
-    right_ctl(0)
+class KeyboardController:
+    def __init__(self, robot: Robot):
+        self.robot = robot
+        if not robot.isOpen():
+            robot.open()
+        robot.enableAllServos(True)
+        self._w_lock = False
+        self._s_lock = False
+        self._d_lock = False
+        self._a_lock = False
+        self._j_lock = False
+        self._k_lock = False
+
+    def init(self):
+        keyboard.on_press_key("w", lambda _: self.key_w_press())
+        keyboard.on_release_key("w", lambda _: self.key_w_release())
+        keyboard.on_press_key("s", lambda _: self.key_s_press())
+        keyboard.on_release_key("s", lambda _: self.key_s_release())
+
+        keyboard.on_press_key("d", lambda _: self.key_d_press())
+        keyboard.on_release_key("d", lambda _: self.key_d_release())
+        keyboard.on_press_key("a", lambda _: self.key_a_press())
+        keyboard.on_release_key("a", lambda _: self.key_a_release())
+
+        keyboard.on_press_key("j", lambda _: self.key_j_press())
+        keyboard.on_release_key("j", lambda _: self.key_j_release())
+
+        keyboard.on_press_key("k", lambda _: self.key_k_press())
+        keyboard.on_release_key("k", lambda _: self.key_k_release())
+        keyboard.on_press_key(" ", lambda _: self.stop())
+
+    def stop(self):
+        print("Stop")
+        self.left_ctl(0)
+        self.right_ctl(0)
+
+    def right_ctl(self, velocity: int):
+        self.robot.doCommand(DynamixelVelocityCommand(12, velocity))
+        self.robot.doCommand(DynamixelVelocityCommand(13, velocity))
+
+    def left_ctl(self, velocity: int):
+        self.robot.doCommand(DynamixelVelocityCommand(11, velocity))
+        self.robot.doCommand(DynamixelVelocityCommand(14, velocity))
+
+    def key_w_press(self):
+        if not self._w_lock:
+            self._w_lock = True
+            self.walk(1)
+
+    def key_w_release(self):
+        self._w_lock = False
+        self.stop()
+
+    def key_s_press(self):
+        if not self._s_lock:
+            self._s_lock = True
+            self.walk(2)
+
+    def key_s_release(self):
+        self._s_lock = False
+        self.stop()
+
+    def key_d_press(self):
+        if not self._d_lock:
+            self._d_lock = True
+            self.walk(3)
+
+    def key_d_release(self):
+        self._d_lock = False
+        self.stop()
+
+    def key_a_press(self):
+        if not self._a_lock:
+            self._a_lock = True
+            self.walk(4)
+
+    def key_a_release(self):
+        self._a_lock = False
+        self.stop()
+
+    def key_j_press(self):
+        if not self._j_lock:
+            self._j_lock = True
+            self.robot.doCommands(getCommandsFromFileName("correct.csv"))
+
+    def key_j_release(self):
+        self._j_lock = False
+        self.stop()
+
+    def key_k_press(self):
+        if not self._k_lock:
+            self._k_lock = True
+            self.robot.doCommands(getCommandsFromFileName("incorrect.csv"))
+
+    def key_k_release(self):
+        self._k_lock = False
+        self.stop()
+
+    def walk(self, direction: int):
+        if direction == 1:
+            self.right_ctl(-VOL)
+            self.left_ctl(VOL)
+            print("walk forward")
+        elif direction == 2:
+            self.right_ctl(VOL)
+            self.left_ctl(-VOL)
+            print("walk backward")
+        elif direction == 3:
+            self.right_ctl(VOL)
+            self.left_ctl(VOL)
+            print("turn right")
+        elif direction == 4:
+            self.right_ctl(-VOL)
+            self.left_ctl(-VOL)
+            print("turn left")
 
 
-def right_ctl(velocity: int):
-    robot.doCommand(DynamixelVelocityCommand(12, velocity))
-    robot.doCommand(DynamixelVelocityCommand(13, velocity))
-
-
-def left_ctl(velocity: int):
-    robot.doCommand(DynamixelVelocityCommand(11, velocity))
-    robot.doCommand(DynamixelVelocityCommand(14, velocity))
-
-
-def key_w_press():
-    global w_lock
-    if not w_lock:
-        w_lock = True
-        walk(1)
-
-
-def key_w_release():
-    global w_lock
-    w_lock = False
-    stop()
-
-
-def key_s_press():
-    global s_lock
-    if not s_lock:
-        s_lock = True
-        walk(2)
-
-
-def key_s_release():
-    global s_lock
-    s_lock = False
-    stop()
-
-
-def key_d_press():
-    global d_lock
-    if not d_lock:
-        d_lock = True
-        walk(3)
-
-
-def key_d_release():
-    global d_lock
-    d_lock = False
-    stop()
-
-
-def key_a_press():
-    global a_lock
-    if not a_lock:
-        a_lock = True
-        walk(4)
-
-
-def key_a_release():
-    global a_lock
-    a_lock = False
-    stop()
-
-
-VOL = 2000
-
-
-def walk(direction: int):
-    if direction == 1:
-        right_ctl(-VOL)
-        left_ctl(VOL)
-        print("walk forward")
-    elif direction == 2:
-        right_ctl(VOL)
-        left_ctl(-VOL)
-        print("walk backward")
-    elif direction == 3:
-        right_ctl(VOL)
-        left_ctl(VOL)
-        print("turn right")
-    elif direction == 4:
-        right_ctl(-VOL)
-        left_ctl(-VOL)
-        print("turn left")
-
-
-while True:
-    pass
+if __name__ == '__main__':
+    ctl = KeyboardController(getRobot())
+    ctl.init()
+    while True:
+        pass
