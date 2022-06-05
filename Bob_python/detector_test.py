761482ee74f66808f0677bd392e66523f41768ec
airobot <airobot@nfu.edu.tw>
2022-06-05 14:56:08 +0800

    Fix 'cannot allocate memory in static TLS block'

--- Bob_python/detector_test.py
+++ Bob_python/detector_test.py
@@ -1,8 +1,10 @@
 import cv2
 
 from Bob.visual.monitor.concrete.crt_camera import CameraMonitor, CameraListener
-from Bob.visual.detector.concrete.face_detect_deepface import FaceDetector
+#Must Import before deepface
 from Bob.visual.detector.concrete.object_detect_yolov5 import ObjectDetector
+# Must Import after yolov5
+from Bob.visual.detector.concrete.face_detect_deepface import FaceDetector
 from Bob.visual.utils import visual_utils
 
 
