ab44bcdc3ac75622119d4418520de73cddf62623
airobot <airobot@nfu.edu.tw>
2022-06-06 23:50:16 +0800

    Fix main.py

--- Bob_python/main.py
+++ Bob_python/main.py
@@ -3,24 +3,20 @@
 Usage:
     $ python path/to/object_detect.py --source path/to/img.jpg --weights yolov5s.pt --img 640
 """
-
-import time
-from Bob.communication.concrete.crt_strategy import ReadLineStrategy
-from constants import CommandControlListener, detector
+from Bob.visual.detector.concrete.face_detect_deepface import FaceDetector
+from Bob.visual.detector.concrete.object_detect_yolov5 import ObjectDetector
+from Bob.visual.monitor.concrete.crt_camera import CameraMonitor
+from constants import CommandControlListener
 from device_config import getSerialBluetooth
 
-package_device = getSerialBluetooth()
-monitor = package_device.getMonitor(CommandControlListener(package_device), ReadLineStrategy())
-monitor.start()
 
 print("Monitor start")
 try:
-    while True:
-        time.sleep(1)
+    camera_monitor = CameraMonitor(0)
+    camera_monitor.registerDetector(FaceDetector(1), False)
+    camera_monitor.registerDetector(ObjectDetector(2, conf=0.4), False)
+    package_device = getSerialBluetooth()
+    package_device.setListener(CommandControlListener(package_device,camera_monitor))
+    package_device.start()
 except (KeyboardInterrupt, SystemExit):
     print("Interrupted!!")
-
-if detector is not None:
-    detector.stop()
-monitor.stop()
-package_device.close()
