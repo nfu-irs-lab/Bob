455c4753d1daa6ad8908a92a52c276d737285ef5
airobot <nfurobotlab.menber@gmail.com>
2022-09-12 13:03:13 +0800

    重構BluetoothConnectionActivity.新增註解.

--- Bob_android/app/src/main/java/com/example/hiwin/teacher_version_bob/activity/BluetoothConnectionActivity.java
+++ Bob_android/app/src/main/java/com/example/hiwin/teacher_version_bob/activity/BluetoothConnectionActivity.java
@@ -11,13 +11,12 @@ import android.support.v7.widget.Toolbar;
 import android.widget.AdapterView;
 import android.widget.ListView;
 import android.widget.Toast;
+
 import com.example.hiwin.teacher_version_bob.DeviceAdapter;
 import com.example.hiwin.teacher_version_bob.R;
 import com.example.hiwin.teacher_version_bob.fragment.ModeDialogFragment;
 
-import java.security.Permission;
 import java.util.ArrayList;
-import java.util.Set;
 
 public class BluetoothConnectionActivity extends AppCompatActivity {
     /*
@@ -31,32 +30,52 @@ public class BluetoothConnectionActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_bluetooth_connection);
         context = this;
+        initializeToolbar();
+        bluetoothInitialize();
+        initializeDeviceList();
+    }
+
+    /**
+     * 新增工具列
+     */
+    private void initializeToolbar() {
         Toolbar myToolbar = (Toolbar) findViewById(R.id.devs_toolbar);
         setSupportActionBar(myToolbar);
+    }
 
+    /**
+     * 初始化並列出已配對之藍芽裝置
+     */
+    private void initializeDeviceList() {
         ListView deviceList = (ListView) findViewById(R.id.devicesList);
-
-        requestPermissions(new String[]{Manifest.permission.BLUETOOTH},1);
-
-        Intent turnOn = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
-        startActivityForResult(turnOn, 0);
-
         BluetoothAdapter bluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
-        deviceList.setAdapter(getDeviceAdaptor(bluetoothAdapter.getBondedDevices()));
+        deviceList.setAdapter(
+                new DeviceAdapter(
+                        context,
+                        new ArrayList<>(
+                                bluetoothAdapter.getBondedDevices())));
         deviceList.setOnItemClickListener(onClickListView);
     }
 
-    private DeviceAdapter getDeviceAdaptor(Set<BluetoothDevice> bondedDevices) {
-        return new DeviceAdapter(this, new ArrayList<>(bondedDevices));
+    /**
+     * 取得藍芽權限，當藍芽未開啟時要求開啟藍芽
+     */
+    private void bluetoothInitialize() {
+        requestPermissions(new String[]{Manifest.permission.BLUETOOTH}, 1);
+        Intent turnOn = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
+        startActivityForResult(turnOn, 0);
     }
 
+    /**
+     * 當裝置被點選時執行之程式
+     */
     private final AdapterView.OnItemClickListener onClickListView = (parent, view, position, id) -> {
         BluetoothDevice selected_device = (BluetoothDevice) parent.getItemAtPosition(position);
         Toast.makeText(BluetoothConnectionActivity.this, selected_device.getName(), Toast.LENGTH_SHORT).show();
 
         ModeDialogFragment newFragment = new ModeDialogFragment();
         newFragment.setListener(mode -> {
-            Intent it=new Intent(context,mode.getSelectedClass());
+            Intent it = new Intent(context, mode.getSelectedClass());
             it.putExtra("address", selected_device.getAddress());
             startActivity(it);
 
