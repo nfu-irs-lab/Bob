447718441e7a850506a1d3c53f9f656545f457a2
airobot <nfurobotlab.menber@gmail.com>
2022-11-23 20:02:02 +0800

    Change InteractiveObjectDetectActivity

--- Bob_android/app/src/main/java/com/example/hiwin/teacher_version_bob/fragment/EntryObjectDetectFragment.java
+++ Bob_android/app/src/main/java/com/example/hiwin/teacher_version_bob/fragment/EntryObjectDetectFragment.java
@@ -3,19 +3,24 @@ package com.example.hiwin.teacher_version_bob.fragment;
 import android.media.MediaPlayer;
 import android.os.Bundle;
 import android.provider.MediaStore;
+import android.speech.tts.TextToSpeech;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.*;
+
 import com.example.hiwin.teacher_version_bob.R;
+
+import java.util.Locale;
 
 
 public class EntryObjectDetectFragment extends StaticFragment {
 
     private String definition;
-    private int audio_id;
+    private TextToSpeech tts;
 
 
     @Nullable
@@ -24,13 +29,15 @@ public class EntryObjectDetectFragment extends StaticFragment {
         View root = inflater.inflate(R.layout.fragment_inter_entry_object_detect, container, false);
         TextView definition = (TextView) root.findViewById(R.id.inter_obj_detect_definition);
         definition.setText(this.definition);
-        MediaPlayer.create(getContext(),audio_id).start();
+        tts.setLanguage(Locale.US);
+        tts.setSpeechRate(0.8f);
+        tts.speak(this.definition, 1, null, "1");
         return root;
     }
 
-    public void setDefinition(String definition, int audio_id) {
+    public void initialize(String definition, TextToSpeech tts) {
         this.definition = definition;
-        this.audio_id = audio_id;
+        this.tts = tts;
     }
 
     @Override
